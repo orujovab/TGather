3510d539a0c734b50e10aab4d33dd94b7056d8bf
orujovab <94193862+orujovab@users.noreply.github.com>
2022-04-25 17:44:57 +0400

    Update post.php

--- includes/post.php
+++ includes/post.php
@@ -1,1 +1,47 @@
+<?php
 
+if(isset($_GET['id']) && $_GET['id'] != $_SESSION['user_id']) {
+    $current_id = $_GET['id'];
+    $flag = 1;
+} else {
+    $current_id = $_SESSION['user_id'];
+    $flag = 0;
+}
+
+echo '<div class="post">';
+echo '<p class="public">';
+// if($flag == 0){ // Message shown if it's your own profile
+//     echo '<a href="edit-post.php?id= ' . $row['post_id'] . ' " class="btn btn-primary"><i class="fas fa-pen" ></i>Edit</a><br>';
+//     echo '<br>';
+// }
+echo '<br>';
+if($row['post_public'] == 'Y') {
+
+    echo 'Public';
+}else {
+
+    echo 'Private';   
+}
+echo '<br>';
+echo '<span class="postedtime">' . $row['post_time'] . '</span>';
+echo '</p>';
+echo '<div>';
+include 'profile_picture.php';
+echo '<a class="profilelink" href="profile.php?id=' . $row['id'] .'">' . $row['username']  . '<a>';
+echo'</div>';
+echo '<br>';
+echo '<p class="caption">' . $row['post_caption'] . '</p>';
+echo '<center>'; 
+
+echo '<br>';
+$target = glob("assets/images/posts/" . $row['post_id'] . ".*");
+if($target) {
+    echo '<img src="' . $target[0] . '" style="max-width:580px">'; 
+    echo '<br><br>';
+}
+echo '<br>';
+echo '</center>';
+echo '</div>';
+
+
+?>
